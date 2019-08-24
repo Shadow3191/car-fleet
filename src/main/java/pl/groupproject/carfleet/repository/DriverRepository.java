0444a10889d1dd63fdd9bb3b7d8a93213dd80dcb
Artur Wróblewski <artur.wroblewski_89@o2.pl>
2019-08-24 11:49:12 +0200

    db working

--- src/main/java/pl/groupproject/carfleet/repository/DriverRepository.java
+++ src/main/java/pl/groupproject/carfleet/repository/DriverRepository.java
@@ -1,8 +1,8 @@
 package pl.groupproject.carfleet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-        import org.springframework.stereotype.Repository;
-        import pl.groupproject.carfleet.model.Driver;
+import org.springframework.stereotype.Repository;
+import pl.groupproject.carfleet.model.Driver;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
